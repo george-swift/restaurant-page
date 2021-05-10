5cf725611a24deaa12ee246e83c5d5afccc7b966
George <71434803+george-swift@users.noreply.github.com>
2021-05-10 17:17:25 +0100

    Refactor index.js to render home and menu pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import home from './home.js';
+import menu from './menu.js';
+import reset from './reset.js';
 import './reset.css';
 import './style.css';
 
 home();
+
+const homeTab = document.querySelector('#home');
+homeTab.onclick = () => {
+  reset();
+  home();
+};
+
+const menuTab = document.querySelector('#menu');
+menuTab.onclick = () => {
+  reset();
+  menu();
+};
